d5711a2afbe334005da9f3e746c0d28cb130635f
gchkadua3 <120250936+gchkadua3@users.noreply.github.com>
2023-05-14 17:53:00 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function firstFunction() {
+    let container = document.getElementById("container");
+    container.style.display="flex";
+}
+
+function secondFunction () {
+    let container = document.getElementById("container");
+    container.style.display="none";
+}
